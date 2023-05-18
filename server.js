302bd41edfb86770f327b9501fc9c5048c97a820
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-18 15:35:06 +0530

    final code of builting multiplayer pong with Socket.I.O

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
+const http = require('http');
+const io = require('socket.io');
+
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+const sockets = require('./sockets');
 
 const PORT = 3000;
+httpServer.listen(PORT);
+console.log(`Listening on port ${PORT}...`);
 
-server.listen(PORT);
-console.log(`Listening on port${PORT}... `);
-
-let readyPlayerCount = 0;
-
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    socket.on('ready', () => {
-        console.log('player ready', socket.id);
-        readyPlayerCount++;
-
-        if (readyPlayerCount === 2) {
-            io.emit('startGame', socket.id);
-        }
-    });
-});
+sockets.listen(socketServer);
